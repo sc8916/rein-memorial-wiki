85599c528c839b0b08386b3c91079304899473c2
Michael Noukhovitch <mnoukhov@gmail.com>
2024-01-12 15:24:41 -0500

    headings working

--- format_html.py
+++ format_html.py
@@ -37,6 +37,7 @@ wikipedia_heading = """
 """
 
 h2_and_h3_tags = text_soup.find_all(["h2", "h3"])
+headings_nested = []
 
 # Replace each <h2> and <h3> with a <span>
 for h_tag in h2_and_h3_tags:
@@ -48,9 +49,59 @@ for h_tag in h2_and_h3_tags:
 
     h_tag.string = ""
     h_tag.append(span)
-    print(span)
+
+    if h_tag.name == "h2":
+        headings_nested.append((name, []))
+    elif h_tag.name == "h3":
+        headings_nested[-1][1].append(name)
 
 start_content.insert_after(text_soup)
+
+start_heading = soup.find("li", {"id": "toc-mw-content-text"})
+
+h2_string = """
+<li class="vector-toc-list-item vector-toc-level-1 vector-toc-list-item-expanded" id="toc-{heading_name}">
+<a class="vector-toc-link" href="#{heading_name}">
+<div class="vector-toc-text">
+<span class="vector-toc-numb">1</span>{heading_name}</div>
+</a>
+<button aria-controls="toc-{heading_name}-sublist" aria-expanded="true" class="cdx-button cdx-button--weight-quiet cdx-button--icon-only vector-toc-toggle">
+<span class="vector-icon vector-icon--x-small mw-ui-icon-wikimedia-expand"></span>
+<span>Toggle {heading_name} subsection</span>
+</button>
+<ul class="vector-toc-list" id="toc-{heading_name}-sublist">
+</ul>
+</li>
+"""
+
+h3_string = """
+<li class="vector-toc-list-item vector-toc-level-2" id="toc-{heading_name}">
+<a class="vector-toc-link" href="#{heading_name}">
+<div class="vector-toc-text">
+<span class="vector-toc-numb">1.3</span>{heading_name}</div>
+</a>
+<ul class="vector-toc-list" id="toc-{heading_name}-sublist">
+</ul>
+</li>
+"""
+
+h2_soups = []
+for h2_name, h3s in headings_nested:
+    h3_soups = []
+    for h3_name in h3s:
+        h3_str = h3_string.format(heading_name=h3_name)
+        h3_soups.append(BeautifulSoup(h3_str, "html.parser"))
+
+    h2_str = h2_string.format(heading_name=h2_name)
+    h2_soup = BeautifulSoup(h2_str, "html.parser")
+    h2_ul = h2_soup.find("ul", {"id": f"toc-{h2_name}-sublist"})
+    for h3_soup in h3_soups:
+        h2_ul.append(h3_soup)
+
+    h2_soups.append(h2_soup)
+
+for h2_soup in reversed(h2_soups):
+    start_heading.insert_after(h2_soup)
 
 with open("index.html", "w") as file:
     file.write(str(soup))
--- text.md
+++ text.md
@@ -1,2 +1,7 @@
 **Rein Otsason** (20 March 1994 in [Toronto](https://en.wikipedia.org/wiki/Toronto), [Canada](https://en.wikipedia.org/wiki/Toronto) - ...) was not an Estonian banker ![alt text](index_files/rein.jpg), but he was a friend, a son, a cousin, a boyfriend, a trivia champion, 
 
+## Early Life
+
+## Career
+
+### Publications
