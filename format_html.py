4706a8a6739b79efce645b8eb9dc534e4f35174e
Michael Noukhovitch <mnoukhov@gmail.com>
2024-01-12 14:54:36 -0500

    format h2 and h3s

--- format_html.py
+++ format_html.py
@@ -12,6 +12,7 @@ with open("text.md", "r") as file:
 
 text_soup = BeautifulSoup(md, "html.parser")
 
+### replace all images with links that show image on hover
 
 image_tags = text_soup.find_all("img")
 
@@ -29,6 +30,25 @@ for i, image_tag in enumerate(image_tags):
     hover_image_soup = BeautifulSoup(hover_image_tag, "html.parser")
     image_tag.replace_with(hover_image_soup)
 
+### format h2 and h1 as wikipedia does
+
+wikipedia_heading = """
+<span class="mw-headline" id="Bank_of_Estonia">Bank of Estonia</span>
+"""
+
+h2_and_h3_tags = text_soup.find_all(["h2", "h3"])
+
+# Replace each <h2> and <h3> with a <span>
+for h_tag in h2_and_h3_tags:
+    name = h_tag.get_text(strip=True)
+    span = soup.new_tag("span")
+    span["class"] = "mw-headline"
+    span["id"] = name
+    span.string = name
+
+    h_tag.string = ""
+    h_tag.append(span)
+    print(span)
 
 start_content.insert_after(text_soup)
 
